4ae23da4b9fb19ef245d6f80911ffd5c322cec75
Zaltman <mariusdruskinis@gmail.com>
2021-10-19 18:11:43 +0300

    added Clear art and square number button

--- scripts.js
+++ scripts.js
@@ -5,18 +5,44 @@ gridContainer.className = 'gridContainer';
 
 body.append(gridContainer);
 
-const gridElement = [];
+const resetBtn = document.createElement('button');
+resetBtn.textContent = 'Clear the art';
+resetBtn.className = 'btn';
+body.insertBefore(resetBtn, gridContainer);
+resetBtn.addEventListener('click', resetArt);
 
-for (let i = 0; i < 256; i++) {
+const gridSetBtn = document.createElement('button');
+gridSetBtn.textContent = 'Set squares number';
+gridSetBtn.className = 'btn';
+body.insertBefore(gridSetBtn, gridContainer);
+
+const gridElement = [];
+const gridColumnElement = [];
+let gridColumns = 20;
+let gridLines = 20;
+//grid creation
+for (let i = 0; i < gridLines; i++) {
   gridElement[i] = document.createElement(`div`);
-  gridElement[i].className = 'grid';
+  gridElement[i].className = 'gridRow';
   gridElement[i].setAttribute('id', i);
 
   gridContainer.append(gridElement[i]);
+  for (let j = 0; j < gridColumns; j++) {
+    gridColumnElement[j] = document.createElement('div');
+    gridColumnElement[j].className = 'gridColumn';
+    gridColumnElement[j].setAttribute('idC', j);
+
+    gridElement[i].append(gridColumnElement[j]);
+  }
 }
+const square = document.querySelectorAll('.gridColumn');
 
 function colorize() {
   this.classList.add('blacked');
 }
 
-gridElement.forEach((div) => div.addEventListener('mouseover', colorize));
+function resetArt() {
+  let ElemsToReset = document.querySelectorAll('.blacked');
+  ElemsToReset.forEach((div) => div.classList.remove('blacked'));
+}
+square.forEach((div) => div.addEventListener('mouseover', colorize));
